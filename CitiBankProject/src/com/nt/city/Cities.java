4644fde0f5608993df2baaeaee040959286ce302
kanakarajujami <kanakarajujami409@gmail.com>
2025-03-18 21:55:20 +0530

    developer added  cities functionality

--- CitiBankProject/src/com/nt/city/Cities.java
+++ CitiBankProject/src/com/nt/city/Cities.java
@@ -5,4 +5,8 @@ public class Cities {
 	public void accept() {
 		System.out.println("Our branches nearly 30 cities there");
 	}
+	
+	public void add() {
+		System.out.println("Comming days branches will open new cities");
+	}
 }
