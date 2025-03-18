b0fa796836e727b35eba976eafd97ecd05327587
kanakarajujami <kanakarajujami409@gmail.com>
2025-03-18 09:41:57 +0530

    developer shop functionality

--- CitiBankProject/src/com/nt/payments/Payments.java
+++ CitiBankProject/src/com/nt/payments/Payments.java
@@ -5,4 +5,8 @@ public class Payments {
 	 public void acceptCards() {
 		 System.out.println("We accept phone pay, google pay, paytm");
 	 }
+	 
+	 public void shop() {
+		 System.out.println("trouser, paint , watch total price is:"+2000);
+	 }
 }
